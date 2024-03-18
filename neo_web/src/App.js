afd0ef6fb49c5ce3a5098346ccfb337e4b5c2a40
Piyamon <piyamon@protonmail.com>
2024-03-18 21:57:41 +0100

    first commit

---
+++ neo_web/src/App.js
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <body className="bg-gradient-to-br from-cyan-500 to-blue-500 box-border h-screen">
+      <div className="flex justify-center ">
+        <h1 className="font-bold text-blue-700 text-7xl mt-8 mb-28">
+          Neogeum
+        </h1>
+      </div>
+
+      <div className="flex justify-center">
+        <h1 className="font-bold text text-blue-700 text-5xl mt-8 mb-28">
+          Le site est en construction
+        </h1>
+        
+      </div>
+      
+      <div className="flex justify-center">
+      <form method="get" action="./download/neogeum-1.1.jar">
+          <button type="submit" className="font-bold text text-blue-700 text-5xl mt-8 mb-28 hover:text-cyan-400">Télécharger</button>
+        </form>
+      </div>
+    </body>
+  );
+}
+
+export default App;
