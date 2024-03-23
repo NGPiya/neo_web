74e92aad2569496a13d39fa3ad43ef202c25d9e9
Piyamon <piyamon@protonmail.com>
2024-03-23 16:05:48 +0100

    add file

--- neo_web/src/App.js
+++ neo_web/src/App.js
@@ -15,7 +15,7 @@ function App() {
       </div>
       
       <div className="flex justify-center">
-      <form method="get" action="./Neo-Launcher.zip">
+      <form method="get" action="./Neo-Launcher-Installer.exe">
           <button type="submit" className="font-bold text text-blue-700 text-5xl mt-8 mb-28 hover:text-cyan-400">Télécharger</button>
         </form>
       </div>
